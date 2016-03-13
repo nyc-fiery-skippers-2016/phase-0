dfc3f1284a8902090fae11c0a033f302be258773
Parker Smathers <parkersmathers@yahoo.com>
2016-03-13 18:45:09 -0400

    add reflection to 5.2

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -49,8 +49,40 @@ end
 
 #p pad([1,2,3], 5)
 
-# 3. Refactored Solution
-
+# 3. Refactored Solution: we only needed to refactor line 42. Originally we had: new_array = []
 
 
 # 4. Reflection
+# Were you successful in breaking the problem down into small steps?
+# Yes.
+
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+# Overall yes. We veered from our pseudocode. My pair introduced the method times, which ended up working out well, because it allowed us to set the difference between min size and length of array, then use that number to add the padding to the array.
+
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+#For the non-destructive, yes. For the destuctive, we had to set the new array equal to Array.new(array), which passed the original array into a new array and changed it permanently.
+
+
+
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+#Array.new(array)
+
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+# It's straitforward. We used destructive and non-destructive for variable names.
+
+
+# What is the difference between destructive and non-destructive methods in your own words?
+# Destructive permanently changes an array, or hash, etc. Non-destructive, the original array can be returned to.
+
+
+
+
+
+
+
+
+
+
+
+
+
